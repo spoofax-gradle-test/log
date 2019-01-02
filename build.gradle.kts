65c42067c280d73fdac37f6e3394f3ba261aa187
Gabriel Konat <gabrielkonat@gmail.com>
2019-01-02 14:03:01 +0100

    Add publishing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,29 @@ plugins {
 
 subprojects {
   group = "org.metaborg"
+  
   repositories {
-    mavenCentral()
+    maven(url = "http://home.gohla.nl:8091/artifactory/all/")
   }
+  
   apply(plugin = "java-library")
   configure<JavaPluginExtension> {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+  }
+
+  apply(plugin = "maven-publish")
+  configure<PublishingExtension> {
+    repositories {
+      maven {
+        name = "Artifactory"
+        url = uri("http://192.168.1.3:8091/artifactory/all/")
+        credentials {
+          username = project.findProperty("publish.repository.Artifactory.username")?.toString()
+          password = project.findProperty("publish.repository.Artifactory.password")?.toString()
+        }
+      }
+    }
   }
 }
 tasks {
