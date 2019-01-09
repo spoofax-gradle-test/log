53c59398bbe98c19e928e75c0a8221bd2925b63f
Gabriel Konat <gabrielkonat@gmail.com>
2019-01-09 16:08:48 +0100

    Use shared configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,11 @@
 plugins {
+  id("org.metaborg.gradle.config.multi-project-root") version "develop-SNAPSHOT"
+  id("org.metaborg.gradle.config.sub-project") version "develop-SNAPSHOT" apply false
   id("org.metaborg.gitonium") version "0.3.0"
 }
 
 subprojects {
-  group = "org.metaborg"
-
-  repositories {
-    maven(url = "http://home.gohla.nl:8091/artifactory/all/")
-  }
-
+  apply(plugin = "org.metaborg.gradle.config.sub-project")
   apply(plugin = "java-library")
-  configure<JavaPluginExtension> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-  }
-
   apply(plugin = "maven-publish")
-  configure<PublishingExtension> {
-    publications {
-      create<MavenPublication>("Default") {
-        from(components["java"])
-      }
-    }
-    repositories {
-      maven {
-        name = "Artifactory"
-        url = uri("http://home.gohla.nl:8091/artifactory/all/")
-        credentials {
-          username = project.findProperty("publish.repository.Artifactory.username")?.toString()
-          password = project.findProperty("publish.repository.Artifactory.password")?.toString()
-        }
-      }
-    }
-  }
-}
-
-tasks {
-  register("buildAll") {
-    dependsOn(subprojects.map { it.tasks["build"] })
-  }
-  register("cleanAll") {
-    dependsOn(subprojects.map { it.tasks["clean"] })
-  }
 }
