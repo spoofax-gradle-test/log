e5f392d44eb6219c307a4fc0b6db5adba8c64ee3
Gabriel Konat <gabrielkonat@gmail.com>
2019-01-10 16:17:48 +0100

    Use MetaBorg Gradle config.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-  id("org.metaborg.gradle.config.multi-project-root") version "develop-SNAPSHOT"
-  id("org.metaborg.gradle.config.sub-project") version "develop-SNAPSHOT" apply false
+  id("org.metaborg.gradle.config") version "0.4.1"
   id("org.metaborg.gitonium") version "0.3.0"
 }
 
 subprojects {
-  apply(plugin = "org.metaborg.gradle.config.sub-project")
-  apply(plugin = "java-library")
-  apply(plugin = "maven-publish")
+  metaborgConfig {
+    configureSubProject()
+    configureJavaLibrary()
+  }
 }
